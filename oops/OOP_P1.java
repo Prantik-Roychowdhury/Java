8d95deee3b6e6e3853c15076537727b58a2565fc
Prantik Roychowdhury <prantikroychowdhury91011@gmail.com>
2025-03-21 11:16:50 +0530

    OPPs Package

---
+++ oops/OOP_P1.java
@@ -0,0 +1,13 @@
+package oops;
+public class OOP_P1
+{
+    int a;
+    public static void main(String[] args)
+    {
+        OOP_P1 p1 = new OOP_P1();
+        OOP_P1 p2 = new OOP_P1();
+        p1.a = 13;
+        p2.a = 123;
+        System.out.println(p1.a);
+    }
+}
