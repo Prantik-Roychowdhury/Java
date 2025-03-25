d207ef01c96ee1289203af0c4cd40d4dc441a7d9
Prantik Roychowdhury <prantikroychowdhury91011@gmail.com>
2025-03-25 14:34:56 +0530

    Method Chaining Package

---
+++ methodChaining/StudentDriver.java
@@ -0,0 +1,12 @@
+package methodChaining;
+
+public class StudentDriver
+{
+    public static void main(String[] args)
+    {
+        Student s = new Student();
+        System.out.println(s.setName("Rohit"));
+        System.out.println(s);
+    }
+    public
+}
