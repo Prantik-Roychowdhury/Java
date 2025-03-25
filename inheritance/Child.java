ce75c8fe5f020dc82886cecafd411f4da3827459
Prantik Roychowdhury <prantikroychowdhury91011@gmail.com>
2025-03-25 15:54:26 +0530

    Final Commit

---
+++ inheritance/Child.java
@@ -0,0 +1,6 @@
+package inheritance;
+
+public class Child extends Parent
+{
+
+}
---
+++ inheritance/ParentChildDriver.java
@@ -0,0 +1,31 @@
+package inheritance;
+
+public class ParentChildDriver
+{
+    //Members that get inherited from Parent Class to Child Class
+    /*
+        * Static Variables
+        * Non-Static Variables
+        * Non-Static Methods
+        * Static Methods
+    */
+    //Members that get inherited from Parent Class to Child Class
+    /*
+        * Constructors
+        * Private Members
+        * Static and Non-Static initializers
+        * */
+    public static void main(String[] args)
+    {
+        //Loading Process with inheritance
+        /*
+        * If the child class has to loaded then both parent and child class has to be loaded into the memory
+        * When the class is loaded into class static area the static initializers will also be loaded
+        */
+        //Constructor Chaining using super() <- statement
+        /*
+        *
+        * */
+        Child c = new Child();
+    }
+}
