172ce0a3c97f729f0bc226f05c3880d9fabe5287
Prantik Roychowdhury <prantikroychowdhury91011@gmail.com>
2025-04-23 09:19:03 +0530

    Final Push of 22/4/25

---
+++ junit/Calculator.java
@@ -0,0 +1,15 @@
+package junit;
+
+public class Calculator
+{
+    int a, b;
+    Calculator(int a, int b)
+    {
+        this.a = a;
+        this.b = b;
+    }
+    public int sum()
+    {
+        return a+b;
+    }
+}
